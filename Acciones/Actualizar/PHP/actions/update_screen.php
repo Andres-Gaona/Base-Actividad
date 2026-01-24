debd57885cdf0c968fe525b9ab56aea142aa012c
Andres-Gaona <147674998+Andres-Gaona@users.noreply.github.com>
2026-01-23 20:30:50 -0600

    Add files via upload

--- Acciones/Actualizar/PHP/actions/update_screen.php
+++ Acciones/Actualizar/PHP/actions/update_screen.php
@@ -4,13 +4,11 @@
     <meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Galileo System</title>
+    <title>Actualizar</title>
     <meta name="robots" content="noindex, nofollow">
-    <meta name="author" content="De la Mora Comunicaciones | www.bydelamora.com">
-    <link rel="shortcut icon" href="../../assets/icons/favicon.ico" />
-    <link rel="apple-touch-icon" href="../../assets/icons/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="../../assets/icons/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="../../assets/icons/apple-touch-icon-114x114.png" />
+    <link rel="shortcut icon" href="" />
+    
+    
     <!-- Bootstrap -->	
       <link href="../../css/reset.css" rel="stylesheet" type="text/css">
 	  <link href="../../css/bootstrap.css" rel="stylesheet">
@@ -18,16 +16,13 @@
       <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
 	
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-      	<script src="../../js/jquery-1.11.3.min.js"></script>
+
+      	<script src="../../js/jquery.min.js"></script>
       </head>
   <body class="total_body">
-  	<!-- body code goes here -->
+
+
+  	<!-- Cuerpo -->
     <div class="total_box delete_box">
       <div class="tittle_box">Actualizar Pantallas</div>
       <form method="post" id="update_screen" name="update_screen">
@@ -41,10 +36,10 @@
           
         </form>  
     </div>
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+  <!-- jQuery --> 
 
       
-	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<!-- Plugins -->
 	 
 	<script src="../../js/bootstrap.js"></script>
       
