ff262d45012b97a8cb6decd1cec3d9e4013deb60
Andres-Gaona <147674998+Andres-Gaona@users.noreply.github.com>
2026-01-23 21:18:47 -0600

    Add files via upload

---
+++ Acciones/Borrar/PHP/actions/delete_activity.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ELIMINAR ACTIVIDAD</title>
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="shortcut icon" href="" />
+  
+    <!-- Bootstrap -->	
+      <link href="../../css/reset.css" rel="stylesheet" type="text/css">
+	  <link href="../../css/bootstrap.css" rel="stylesheet">
+	  <link href="../css/floats.css" rel="stylesheet" type="text/css">
+      <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+	
+  </head>
+  <body class="total_body">
+  	<!-- Cuerpo -->
+    <div class="total_box delete_box">
+      <div class="tittle_box">Eliminar Actividad</div>
+      <form method="post" id="delete_activity" name="delete_activity">
+        <div class="value_box"></div>
+        <input name="id_activity" type="hidden" id="id_activity" value="">
+        <div><a class="button_form green_button" href="javascript:parent.$.fancybox.close();">Cancelar</a><input name="delete_activity" type="submit" class="button_form red_button" id="delete_activity" value="Eliminar"></div>
+        </form>  
+    </div>
+
+  <!-- jQuery  --> 
+	<script src="../../js/jquery.min.js"></script>
+
+	<!-- Plugins -->
+	 
+	<script src="../../js/bootstrap.js"></script>
+  </body>
+</html>
